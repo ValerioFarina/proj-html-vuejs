9b2eda89314aaacfce8af49d130bd3fcdf20f04f
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-02 17:20:57 +0100

    Vue: inserite dinamicamente le cards nella sezione relativa ai corsi

--- main.js
+++ main.js
@@ -48,6 +48,57 @@ var app = new Vue({
             'Get the desired certificate delivered at house'
         ],
 
+        courses : [
+            [
+                {
+                    img : 'img/course-02-480x298.jpg',
+                    price : '$40.00',
+                    name : 'Learning To Write As A Professional Author',
+                    lessons : 20,
+                    students : 50
+                },
+                {
+                    img : 'img/stock-full-hd-03-480x298.jpg',
+                    price : 'Free',
+                    name : 'Customer-centric-Info-Tech Strategies',
+                    lessons : 24,
+                    students : 769
+                }
+            ],
+            [
+                {
+                    img : 'img/stock-full-hd-04-480x298.jpg',
+                    price : '$19.00',
+                    name : 'Open Programming Courses for Everyone: Python',
+                    lessons : 17,
+                    students : 62
+                },
+                {
+                    img : 'img/stock-full-hd-06-480x298.jpg',
+                    price : '$26.00',
+                    name : 'Academic Listening and Note-taking',
+                    lessons : 14,
+                    students : 67
+                }
+            ],
+            [
+                {
+                    img : 'img/course-featured-image-01-480x298.jpg',
+                    price : '$39.00',
+                    name : 'Master JQuery in a Short Period of Time',
+                    lessons : 61,
+                    students : 51
+                },
+                {
+                    img : 'img/stock-full-hd-05-480x298.jpg',
+                    price : '$59.00',
+                    name : 'Introduction to Javascript for Beginners',
+                    lessons : 14,
+                    students : 76
+                }
+            ]
+        ],
+
         footerMenus : [
             {
                 title : 'Explore',
