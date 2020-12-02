787131e6e704b2f614b1507208ae441bf9871eb0
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-02 09:28:22 +0100

    Vue: aggiunte dinamicamente le voci dei menu presenti nel footer

--- main.js
+++ main.js
@@ -39,6 +39,44 @@ var app = new Vue({
 
                 ]
             }
+        ],
+
+        footerMenus : [
+            {
+                title : 'Explore',
+                items : [
+                    'Start here',
+                    'Success story',
+                    'Blog',
+                    'Courses',
+                    'About us',
+                    'Contact us'
+                ],
+                classes : [
+                    'd-flex',
+                    'flex-wrap'
+                ]
+            },
+            {
+                title : 'Information',
+                items : [
+                    'Membership',
+                    'Purchase guide',
+                    'Privacy Policy',
+                    'Terms of services'
+                ],
+                classes : [
+
+                ]
+            }
         ]
+    },
+
+    methods : {
+        // this function calculates how many columns should be inserted in the right part of the footer
+        // the number of columns depends on the number of elements contained in the array footerMenus
+        colNumber() {
+            return 'col-' + (12 / this.footerMenus.length);
+        }
     }
 });
