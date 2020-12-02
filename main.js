5782d1e258ab0766ed8af224259f2c399d42086e
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-02 09:06:22 +0100

    Vue: aggiunte dinamicamente le voci del menu dell'header

---
+++ main.js
@@ -0,0 +1,44 @@
+var app = new Vue({
+    el : '#root',
+
+    data : {
+        navMenu : [
+            {
+                title : 'Home',
+                subMenu : [
+
+                ]
+            },
+            {
+                title : 'Pages',
+                subMenu : [
+
+                ]
+            },
+            {
+                title : 'Courses',
+                subMenu : [
+
+                ]
+            },
+            {
+                title : 'Features',
+                subMenu : [
+
+                ]
+            },
+            {
+                title : 'Blog',
+                subMenu : [
+
+                ]
+            },
+            {
+                title : 'Shop',
+                subMenu : [
+
+                ]
+            }
+        ]
+    }
+});
